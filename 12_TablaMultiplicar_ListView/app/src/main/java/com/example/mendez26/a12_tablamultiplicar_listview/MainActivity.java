1f02706e19d8ec0796d2206fc63503e56d9bd09e
Kevin Méndez <kealmendezsa@ittepic.edu.mx>
2018-02-10 22:37:51 -0700

    Actividad 1.2 Tablas de multiplicar usando un seekBar

--- 12_TablaMultiplicar_ListView/app/src/main/java/com/example/mendez26/a12_tablamultiplicar_listview/MainActivity.java
+++ 12_TablaMultiplicar_ListView/app/src/main/java/com/example/mendez26/a12_tablamultiplicar_listview/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     public EditText pan;
 
+    private TextView mostrarPorcentaje;
+    private SeekBar barra;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,14 +34,41 @@ public class MainActivity extends AppCompatActivity {
         tablaMultiplicar = (ListView) findViewById(R.id.tabla);
         tablaMultiplicar.setAdapter(adaptador);
 
-        pan = (EditText) findViewById(R.id.caja);
+        mostrarPorcentaje = (TextView)findViewById(R.id.txtCargar);
+
+        // SeekBar
+        barra = (SeekBar)findViewById(R.id.seekBar);
+        // Valor Inicial
+        barra.setProgress(0);
+        // Valot Final
+        barra.setMax(19);
+        barra.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    //hace un llamado a la perilla cuando se arrastra
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar,
+                                                  int progress, boolean fromUser) {
+                        int cantidad = Integer.parseInt(String.valueOf(progress));
+                        mostrarPorcentaje.setText((cantidad+1)+"");
+                        tablaMultiplicar(cantidad+1);
+                    }
+
+                    //hace un llamado  cuando se toca la perilla
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    //hace un llamado  cuando se detiene la perilla
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                });
+
+        //pan = (EditText) findViewById(R.id.caja);
 
     }//onCreate
 
-    public void tablaMultiplicar(View v){
+    public void tablaMultiplicar(int numero){
 
         limpiarTabla();
-        int numero = Integer.parseInt(pan.getText().toString());
 
         for(int i = 1; i <= 20; i++){
 
