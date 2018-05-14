e0d9359848c2d94e8659eeec8a38f70edefd74e8
Kevin Méndez <kealmendezsa@ittepic.edu.mx>
2018-05-14 00:10:34 -0600

    Actividad 5.4 Contador de pasos

---
+++ 54_Contador_de_Pasos/app/src/main/java/com/example/mendez26/a54_contador_de_pasos/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.mendez26.a54_contador_de_pasos;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener{
+
+    private SimpleStepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private static final String TEXT_NUM_STEPS = "";
+    private int numSteps;
+    private TextView step;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_pedometer);
+
+        step = (TextView) findViewById(R.id.textView);
+
+        // Get an instance of the SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new SimpleStepDetector();
+        simpleStepDetector.registerListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        numSteps = 0;
+        step.setText(TEXT_NUM_STEPS + numSteps);
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    sensorEvent.timestamp, sensorEvent.values[0], sensorEvent.values[1], sensorEvent.values[2]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void step(long timeNs) {
+        numSteps++;
+        step.setText(TEXT_NUM_STEPS + numSteps);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    public void reiniciarContador(View view){
+        numSteps = 0;
+        step.setText(TEXT_NUM_STEPS + numSteps);
+    }
+
+}
---
+++ 54_Contador_de_Pasos/app/src/main/java/com/example/mendez26/a54_contador_de_pasos/StepListener.java
@@ -0,0 +1,5 @@
+package com.example.mendez26.a54_contador_de_pasos;
+
+public interface StepListener {
+    public void step(long timeNs);
+}
